cd18317012c53169abeb43c7b5886d6a8854c10e
ChenHe <smilehe0131@outlook.com>
2017-10-30 22:11:50 +0800

    修改bug，添加个人中心

--- Club/Controllers/UserController.cs
+++ Club/Controllers/UserController.cs
@@ -5,32 +5,84 @@ using System.Web;
 using System.Web.Mvc;
 using Club.Models;
 using System.Data.Entity;
+using Webdiyer.WebControls.Mvc;
+using Microsoft.Ajax.Utilities;
+using static Club.AuthFilter;
 
 namespace Club.Controllers
 {
-    public class UserController : BaseController
+    public class UserController:BaseController
     {
+        static int key = 1;
         /// <summary>
         /// 用户页面
         /// </summary>
         /// <returns></returns>
         // GET: Login
-        public ActionResult Index()
-        {
-            var userid = Request["userid"] ?? "";
-            int id = userid.ToInt();
-            if(id!=0)
-            {
-                using (var db=new Entities())
-                {
-                    var user = db.User.OrderByDescending(a => a.Id).Include(a => a.Level).FirstOrDefault(a => a.Id == id);
-                    Session["browseuser"] = user;
+        [UserCheck(IsNeed = false)]
+        public ActionResult Index(int? id) {
+            int userid = Request["userid"].ToInt();
+            var user = (User)Session["loginuser"];
+            int requestkey = Request["key"].ToInt();
+            if(requestkey != 0) {
+                key = requestkey;
+            }
+            var pageIndex = id ?? 1;
+            int pageSize = 10;
+            PagedList<Post> pageListpost;
+            PagedList<Collection> pageListcollection;
+            PagedList<Reply> pageListreply;
+            using(var db = new Entities()) {
+                //判断用户访问自己的信息还是访问其他用户的信息
+                if(userid <= 0) {
+                    if(user != null) {
+                        userid = user.Id;
+                    }
+                    else {
+                        ShowMassage("未知错误");
+                        return Redirect("/Post/Index");
+                    }
+                }
+                else {
+                    user = db.User.Include(a => a.Level).FirstOrDefault(a => a.Id == userid);
+                }
+                ViewBag.User = user;
+                //帖子数量
+                var post = db.Post.OrderByDescending(a => a.Id).Where(a => a.UserId == userid);
+                ViewBag.postnumber = post.Count();
+                switch(key) {
+                    //帖子
+                    case 1:
+                        ViewBag.url = "_MyPost";
+                        pageListpost = post.ToPagedList(pageIndex: pageIndex,pageSize: pageSize);
+                        if(Request.IsAjaxRequest()) {
+                            return PartialView("_MyPost",pageListpost);
+                        }
+                        return View(pageListpost);
+                    //回复
+                    case 2:
+                        ViewBag.url = "_MyReply";
+                        var listreply = new List<ReplyModel>();
+                        var reply = db.Reply.OrderByDescending(a => a.Id).Include(a => a.Post).Include(a => a.User).Where(a => a.UserId == userid).DistinctBy(a => a.PostId);
+                        pageListreply = reply.ToPagedList(pageIndex: pageIndex,pageSize: pageSize);
+                        if(Request.IsAjaxRequest()) {
+                            return PartialView("_MyReply",pageListreply);
+                        }
+                        break;
+                    //收藏
+                    case 3:
+                        ViewBag.url = "_MyDynamic";
+                        var collection = db.Collection.OrderByDescending(a => a.Id).Include(a => a.Post).Where(a => a.UserId == userid);
+                        pageListcollection = collection.ToPagedList(pageIndex: pageIndex,pageSize: pageSize);
+                        if(Request.IsAjaxRequest()) {
+                            return PartialView("_MyDynamic",pageListcollection);
+                        }
+                        break;
+                    //粉丝
+                    case 4:
+                        break;
                 }
             }
-            return View();
-        }
-        public ActionResult Dynamic()
-        {
             return View();
         }
         /// <summary>
@@ -38,30 +90,24 @@ namespace Club.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult Login()
-        {
+        public ActionResult Login() {
             return View();
         }
         [HttpPost]
-        public ActionResult Login(UserLogin UserLogin)
-        {
-            if (string.IsNullOrEmpty(UserLogin.Account) || string.IsNullOrEmpty(UserLogin.Password))
-            {
-                TempData["login"] = "用户名或密码不能为空";
+        public ActionResult Login(UserLogin UserLogin) {
+            if(string.IsNullOrEmpty(UserLogin.Account) || string.IsNullOrEmpty(UserLogin.Password)) {
+                ShowMassage("用户名或密码不能为空");
                 return RedirectToAction("Login");
             }
-            using (var club = new Entities())
-            {
+            using(var club = new Entities()) {
                 UserLogin.Password = UserLogin.Password.MD5Encoding(UserLogin.Account);
-                var user = club.User.OrderByDescending(a => a.Id).Include(a =>a.Level).FirstOrDefault(a => a.Account == UserLogin.Account);
-                if (user == null)
-                {
-                    TempData["login"] = "用户名不存在";
+                var user = club.User.OrderByDescending(a => a.Id).Include(a => a.Level).FirstOrDefault(a => a.Account == UserLogin.Account);
+                if(user == null) {
+                    ShowMassage("用户名不存在");
                     return RedirectToAction("Login");
                 }
-                if (user.PassWord != UserLogin.Password)
-                {
-                    TempData["login"] = "密码错误";
+                if(user.PassWord != UserLogin.Password) {
+                    ShowMassage("密码错误");
                     TempData["account"] = UserLogin.Account;
                     return RedirectToAction("Login");
                 }
@@ -75,20 +121,15 @@ namespace Club.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult Register()
-        {
+        public ActionResult Register() {
             return View();
         }
         [HttpPost]
-        public ActionResult Register(UserRegister UserRegister)
-        {
-            using (var db=new Entities())
-            {
+        public ActionResult Register(UserRegister UserRegister) {
+            using(var db = new Entities()) {
                 var user = db.User.ToList();
-                foreach(var item in user)
-                {
-                    if(item.Account==UserRegister.Account)
-                    {
+                foreach(var item in user) {
+                    if(item.Account == UserRegister.Account) {
                         ShowMassage("账号已存在");
                         return View();
                     }
@@ -109,8 +150,7 @@ namespace Club.Controllers
         /// 用户退出
         /// </summary>
         /// <returns></returns>
-        public ActionResult Introduction()
-        {
+        public ActionResult Introduction() {
             Session["loginuser"] = null;
             return Redirect("/Post");
         }
